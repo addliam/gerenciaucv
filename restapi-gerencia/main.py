6444a420466576af68dca6e72b53c381d94bc65d
codeli4m <liamyostin2004@gmail.com>
2024-10-28 16:36:46 -0500

    readme

--- restapi-gerencia/main.py
+++ restapi-gerencia/main.py
@@ -46,8 +46,3 @@ def read_root():
     response = gpt.answer(
         "Quién fue el primer presidente de los Estados Unidos")
     return {"response": response}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
